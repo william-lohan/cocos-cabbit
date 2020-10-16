9a20cef054cf5237810c84900f75160196330e16
William Lohan <william.g.lohan@gmail.com>
2020-10-16 13:20:36 -0700

    init

---
+++ assets/Tiled/cave.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="cave" tilewidth="16" tileheight="16" tilecount="460" columns="20">
+ <image source="../cabbit-0.5/tileset/PNG/16x16/cave.png" width="320" height="368"/>
+ <terraintypes>
+  <terrain name="GrassOnDirt" tile="260"/>
+ </terraintypes>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="21" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="24" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="41" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="43">
+  <animation>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="44" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="83">
+  <animation>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="123" duration="100"/>
+   <frame tileid="143" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="260" terrain=",,,0"/>
+ <tile id="261" terrain=",,0,0"/>
+ <tile id="262" terrain=",,0,"/>
+ <tile id="280" terrain=",0,,0"/>
+ <tile id="281" terrain="0,0,0,0"/>
+ <tile id="282" terrain="0,,0,"/>
+ <tile id="300" terrain=",0,,"/>
+ <tile id="301" terrain="0,0,,"/>
+ <tile id="302" terrain="0,,,"/>
+</tileset>
